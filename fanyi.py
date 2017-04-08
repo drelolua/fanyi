9affb76070ee164c872ae842f456cc80fcb4e3a9
崔士川 <cuisc13@gmail.com>
2017-04-08 19:21:03 +0800

    a 添加 翻译记录存储功能

--- fanyi.py
+++ fanyi.py
@@ -5,10 +5,15 @@
 
 import json
 import sys
+import os
 import urllib2
+import sqlite3
 from urllib2 import HTTPError
+import time
 
 url = "https://sp1.baidu.com/5b11fzupBgM18t7jm9iCKT-xh_/sensearch/selecttext"
+home = os.environ['HOME']
+db_path = home+"/.config/fanyi/fanyi.db"
 
 def fanyi(q):
     q = q
@@ -21,22 +26,53 @@ def fanyi(q):
 
     return jso
 
+def save(data):
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+    fmt = u'''
+    insert into log
+    (keyword, resp, resp_jso, at_int, at_str)
+    values
+    ('{0[key]}', '{0[resp]}','{0[resp_jso]}', {0[at_int]}, '{0[at_str]}')
+    ;
+    '''
+    sql = fmt.format(data)
+    cur.execute(sql)
+    cur.close()
+    conn.commit()
+    conn.close()
+
+
+
 if __name__ == '__main__':
     q = ''
+    qargs = ''
+    s = ''
+    t = time.time()
     try:
         qargs = sys.argv[1]
         q = '+'.join(qargs.split(' '))
     except IndexError as e:
-        print('No word to fanyi')
+        s = 'No word to fanyi'
     jso = fanyi(q)
     if jso['errno']==1:
-        print(jso['error'])
+        s = jso['error']
     else:
         result = jso['data']['result']
         #if isinstance(result,type([])):
         if jso['data']['type']==1:
             for i in result:
-                print("pre.%s " % i['pre'])
-                print("cont. %s\n"% i['cont'])
+                s = "pre.%s \n" % i['pre']
+                s += "cont. %s\n"% i['cont']
         else:
-            print("大概要查的是: %s"% result.encode('utf-8'))
+            s = "大概要查的是: %s"% result.encode('utf-8')
+    print(s)
+    data = {
+        'key':qargs,
+        'resp':s.replace('\n', ' '),
+        'resp_jso':json.dumps(jso),
+        'at_int':int(t),
+        'at_str':time.strftime(u"%Y-%m-%d %H:%M:%S", time.localtime(t))
+
+    }
+    save(data)
---
+++ savefan.sql
@@ -0,0 +1,8 @@
+create table log(
+    id integer primary key autoincrement,
+    keyword text,
+    resp text,
+    resp_jso text,
+    at_int integer,
+    at_str text
+);
